12ca9fd9297aefb2023036e326de6519aa75720e
WilmerSaavedra <wilmee409@gmail.com>
2020-04-26 18:37:32 -0500

    se agrega fecha a la clase Tarea

--- TareaProyecto/src/Tarea.java
+++ TareaProyecto/src/Tarea.java
@@ -1,9 +1,12 @@
-
+import java.util.Date;
 public class Tarea {
 	
 	
 	public static void main(String[] arg)
 	{
 		System.out.println("Alexander Tincopa");
+           //fecha
+           Date fecha = new Date();
+	       System.out.println ("hoy es : "+fecha);
 	}
 }
